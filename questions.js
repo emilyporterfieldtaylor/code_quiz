a416c5ee7afdc2e7bc1b9f5b2620dc3b2f27ab12
Emily Taylor <emilyporterfieldtaylor@gmail.com>
2020-01-11 17:10:55 -0600

    create questions in question.js

---
+++ questions.js
@@ -0,0 +1,27 @@
+var questions = [
+  {
+    title: "Commonly used data types DO NOT include:",
+    choices: ["strings", "booleans", "alerts", "numbers"],
+    answer: "alerts"
+  },
+  {
+    title: "The condition in an if / else statement is enclosed within ____.",
+    choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
+    answer: "parentheses"
+  },
+  {
+    title: "What does JSON stand for?",
+    choices: ["javascript octopus notation", "jessicasimpson object notation", "javascript object nutella", "javascript object notation"],
+    answer: "javascript object notation"
+  },
+  {
+    title: "Which of these is NOT a front-end development language?",
+    choices: ["html", "sql", "css", "javascript"],
+    answer: "sql"
+  },
+  {
+    title: "Which of these symbols is the css class selector?",
+    choices: [".", "!", "<>", "#"],
+    answer: "."
+  }
+];
